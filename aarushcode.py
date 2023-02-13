dc0a8c4548aff4d7a125e530bae8daba3898dfa1
aarush-g123 <reachaarush01@gmail.com>
2023-02-13 11:33:08 -0500

    created a new file

--- aarushcode.py
+++ aarushcode.py
@@ -12,19 +12,17 @@ clock = pygame.time.Clock()
 red = (255,0,0)
 black = (0,0,0)
 
-background = pygame.image.load("assets/images/background.gif")
-bg_width = background.get_rect().width
-bg_height =background.get_rect().height
-# background = pygame.transform.scale(background, (bg_width/1.7, bg_height/1.3))
+basket = pygame.image.load("assets/images/basket.png")
+basket_width = basket.get_rect().width
+basket_height =basket.get_rect().height
 
-# base = pygame.image.load("assets/images/basketball.png")
-# base_width = base.get_rect().width
-# base_height = base.get_rect().height
-# base = pygame.transform.scale(base, (base_width*2.6, base_height*2.6))
 ball = pygame.image.load("assets/images/basketball.png")
 ball_width = ball.get_rect().width
 ball_height = ball.get_rect().height
 ball = pygame.transform.scale(ball, (ball_width/16.5, ball_height/16.5))
+
+bg_img = pygame.image.load("assets/images/bg.png")
+bg_img = pygame.transform.scale(bg_img,(700,700))
 
 def main():
     gameOn = True
@@ -56,7 +54,7 @@ def main():
                 if event.key == pygame.K_SPACE:
                     bouncing = 25
 
-        screen.blit(background, (0, 0))
+        screen.blit(basket, (0, 0))
         # screen.blit(base, (baseX, baseY))
         screen.blit(ball, (ballX, ballY))
 
