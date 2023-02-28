132fb3e9afddbe2509c074747a2d9574330a5980
dean1210e <deanelbayar@gmail.com>
2023-02-27 22:51:59 -0500

    continued the main menu!

--- mainmenu.py
+++ mainmenu.py
@@ -1,22 +1,45 @@
-from tkinter import *
-from AidenCode import basketballgame
+import pygame
+import button
 
-class Application(Frame):
-    def __init__(self, master):
-        super().__init__(master)
-        self.grid()
-        self.image_start = PhotoImage(file = "midyear-project/assets/images/startbutton.png")
-        self.create_widgets()
+screen_height = 900
+screen_width= 1440
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Button Demo")
+
+background = pygame.image.load('assets/images/menubg.jpg')
+logo = pygame.image.load('assets/images/basketball1.png')
+title = pygame.image.load('assets/images/title.png')
+names = pygame.image.load('assets/images/names.png')
+
+start_img = pygame.image.load('assets/images/playbutton.png').convert_alpha()
+quit_img = pygame.image.load('assets/images/quitbutton.png').convert_alpha()
+
+
+start_button = button.Button(400, 300, start_img, 1)
+quit_button = button.Button(700, 300, quit_img, 1)
+
+
+run = True
+while run:
         
-    
-    def create_widgets(self):
-        Label(self, text= "Click start to begin").grid(row=0, column = 0, columnspan = 5, sticky = N)
+        # screen.fill((135, 206, 235))
+        screen.fill((0,0,0))
+        screen.blit(background,(0,0))
+        screen.blit(title,(450,0))
+        screen.blit(names,(500,75))
+        # screen.blit(logo,(0,0))
 
+        if start_button.draw(screen):
+                print("START")
+                
+        if quit_button.draw(screen):
+                run = False
 
-        Button(self, text = "start", command = self.basketballgame(), image = self.image_start).grid(row=1, column=0)
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        run = False
 
+        pygame.display.update()
 
-root = Tk()
-root.title("basketballmenu")
-view = Application
-root.mainloop()
+pygame.quit()
