6fff93d31199556fdc4ab3726368755ef5e98557
dean1210e <deanelbayar@gmail.com>
2023-03-03 13:56:09 -0500

    FINALLY FINISHED THE HOVERS!!!

--- mainmenu.py
+++ mainmenu.py
@@ -1,5 +1,6 @@
 import pygame
-import button
+import playbutton
+import quitbutton
 
 screen_height = 900
 screen_width= 1440
@@ -12,13 +13,11 @@ title = pygame.image.load('assets/images/title.png')
 names = pygame.image.load('assets/images/names.png')
 
 start_img = pygame.image.load('assets/images/playbutton.png').convert_alpha()
-#playhover = pygame.image.load('assets/images/playhover.png').convert_alpha()
-#quithover = pygame.image.load('assets/images/quithover.png').convert_alpha()
 quit_img = pygame.image.load('assets/images/quitbutton.png').convert_alpha()
 
 
-start_button = button.Button(575, 300, start_img,  1)
-quit_button = button.Button(575, 400, quit_img, 1)
+start_button = playbutton.Button(575, 300, start_img,  1)
+quit_button = quitbutton.Button(575, 400, quit_img, 1)
 
 run = True
 while run:
